17431d9e0e8e996db08bf4778d9fc9158f72bbff
DrugLog <androiddruglog@gmail.com>
2023-05-06 07:47:22 -0400

    changed port

--- src/config/server.js
+++ src/config/server.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
-// const PORT = 8080;
+// const PORT = 3000;
+const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
