e70db98387daacdbedfeac7f3baaf09445694cd9
DrugLog <androiddruglog@gmail.com>
2023-05-06 06:51:29 -0400

    Initial commit

---
+++ src/config/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.listen(PORT, () => {
+    console.log('Server is listen at http://localhost:' + PORT);
+});
+
+app.get('/api/testserver', async (req, res) => {
+    res.send({
+        result: "ok"
+    });
+});
