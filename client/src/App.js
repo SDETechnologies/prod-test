ad07c93cce21de98244d465e00e384b6cda1e632
DrugLog <androiddruglog@gmail.com>
2023-05-06 07:36:01 -0400

    Added fetch for server in client

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,14 @@ import './App.css';
 function App() {
 
   
-  var testServer = () => {
-    console.log('testServer');
+  var testServer = async () => {
+    console.log('testServer()');
+    await fetch('https://cra-cloud-run-snc4sjprxq-uc.a.run.app/api/testserver')
+    .then(res => res.json())
+    .then(data => {
+      console.log('data: ', data);
+    })
+    .catch(err => console.log('error: ', error));
   }
   
 
