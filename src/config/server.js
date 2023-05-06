e063f85b98e8872d7d5f6003e7eb875188092cfa
DrugLog <androiddruglog@gmail.com>
2023-05-06 08:08:50 -0400

    Test

--- src/config/server.js
+++ src/config/server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({
     extended: true,
 }));
 
+var testPath = path.join(__dirname,"../../client/build");
+// console.log('testPath: ', testPath);
+app.use(express.static(testPath));
+
 app.listen(PORT, () => {
     console.log('Server is listen at http://localhost:' + PORT);
 });
