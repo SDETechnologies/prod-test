0f792e262f1fbbd8ef6b2777c9e2f67e47ae4945
DrugLog <androiddruglog@gmail.com>
2023-05-06 07:04:55 -0400

    Added basic server and client code

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3002;
+const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
